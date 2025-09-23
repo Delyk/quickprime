fa21e21c87714759abc77374ff2f5372c157e70a
Delyk <homahomovic3500@gmail.com>
2025-09-23 16:49:42 +0400

    Add parallel

--- src/prime.cpp
+++ src/prime.cpp
@@ -6,19 +6,19 @@
 #include <thread>
 #include <vector>
 
-std::mutex mut;
-static void cut_non_primes(std::vector<uint_fast64_t> &lp,
-                           const std::vector<uint_fast64_t> &pr,
+static std::mutex mut;
+static void cut_non_primes(std::vector<uint_fast64_t> &lp, uint_fast64_t p,
                            const std::size_t &i, const uint_fast64_t &n) {
-  for (auto p = pr.begin(); *p <= lp[i] && *p * i <= n; p++) {
-    std::lock_guard<std::mutex> m(mut);
-    lp[*p * i] = *p;
-  }
+  std::lock_guard<std::mutex> m(mut);
+  lp[p * i] = p;
 }
 
 void prime::sieve_linear(uint_fast64_t n) {
   std::size_t thr_count = std::thread::hardware_concurrency();
-  std::vector<std::thread> threads(thr_count);
+  if (!thr_count) {
+    thr_count = 2;
+  }
+  std::vector<std::thread> threads;
   std::vector<uint_fast64_t> pr;
   std::vector<uint_fast64_t> lp(n + 1, 0);
   for (std::size_t i = 2; i <= n; i++) {
@@ -27,9 +27,29 @@ void prime::sieve_linear(uint_fast64_t n) {
       pr.push_back(i);
     }
     for (auto p = pr.begin(); *p <= lp[i] && *p * i <= n; p++) {
-      lp[*p * i] = *p;
+      threads.emplace_back(cut_non_primes, std ::ref(lp), *p, std ::ref(i),
+                           std ::ref(n));
+      if (threads.size() >= thr_count) {
+        for (auto &t : threads) {
+          if (t.joinable()) {
+            t.join();
+          }
+        }
+        threads.clear();
+      }
     }
+    for (auto &t : threads) {
+      if (t.joinable()) {
+        t.join();
+      }
+    }
+    threads.clear();
   }
+  for (auto &t : threads) {
+    if (t.joinable())
+      t.join();
+  }
+  threads.clear();
   for (auto i : pr) {
     std::cout << i << std::endl;
   }
